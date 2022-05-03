b9e305ffc3792fb73bd5e73747cf18e8b4b9479f
Anton <tony.osmolovsky@gmail.com>
2022-05-03 18:18:47 +0200

    Add class with exception

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+package classwork13;
+
+public class MyClass {
+	public void someMethod(int a, int b) throws ArithmeticException { // let the next one catch
+		/*try {
+			System.out.println(a/b);
+		} catch (Exception e) {
+			System.out.println("Exception");
+		}*/
+		System.out.println(a/b);
+		
+	}
+}
