7060835e7c495479d0ee4e5619390db374725770
Anton <tony.osmolovsky@gmail.com>
2022-05-03 18:22:41 +0200

    Add main class for Sql lite connection

---
+++ SqLiteMain.java
@@ -0,0 +1,12 @@
+package classwork13;
+
+public class SqLiteMain {
+
+	public static void main(String[] args) {
+		
+		SqLiteConnector con = new SqLiteConnector();
+		con.getConnection();
+		
+	}
+
+}
